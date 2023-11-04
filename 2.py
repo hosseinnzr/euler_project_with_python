edaf340d60cdf61c52d65ce00eefb3aa12bc0a9a
hossein nazari <nazari.hossein28@gmail.com>
2023-11-04 15:19:11 +0330

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,15 @@
+first = 0
+second = 1
+x = 0
+sum = 0
+for i in range(1, 10000000):    
+    if x < 4000000 :
+        x = first + second
+        first = second
+        second = x
+        i +=1
+        if x % 2 == 0 :
+            sum += x
+    else : 
+        break
+print(sum)
