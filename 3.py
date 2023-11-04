438cfb8110de8f2f0a8f27c5f7b9249de0be0f4c
hossein nazari <nazari.hossein28@gmail.com>
2023-11-04 15:19:27 +0330

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,16 @@
+def avall(m):
+    j=0
+    for i in range(1, m+1):
+        if m % i == 0 :
+            j += 1
+    if j == 2 :
+        return True
+    else:
+        return False  
+
+def prime_factors(n):
+    for i in range(1, n+1):
+        if n % i == 0 and avall(i):
+            print (i)
+
+prime_factors (600851475143)
