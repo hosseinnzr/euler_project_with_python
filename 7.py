66d3baee85d616b88e65c11616391a4b2a856656
hossein nazari <nazari.hossein28@gmail.com>
2024-03-13 10:23:55 +0330

    euler project number 7
    euler project number 7

--- 7.py
+++ 7.py
@@ -1,21 +1,26 @@
-def isPrime(n):
-    if n < 2: return "Neither prime, nor composite"
-    print(n)
-    for i in range(2, int(n**0.5) + 1):
-        print(i)
-        if n % i == 0:
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
             return False
+        i += 6
     return True
 
-# returns the nth prime number
-def nthPrime(n):
-    numberOfPrimes = 0
-    prime = 1
+def find_nth_prime(n):
+    prime_count = 1
+    num = 1
+    while prime_count < n:
+        num += 2
+        if is_prime(num):
+            prime_count += 1
+    return num
 
-    while numberOfPrimes < n:
-        prime += 1
-        if isPrime(prime):
-            numberOfPrimes += 1
-    return prime
-
-print(nthPrime(10001))
+nth_prime = 10001
+result = find_nth_prime(nth_prime)
+print("The", nth_prime, "st prime number is:", result)
